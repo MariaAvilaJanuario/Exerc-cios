b0255bed9851ab5477e69aeee84bdda066c9c51e
Duda <maria.avila.januario@escola.pr.gov.br>
2026-03-17 11:21:36 -0300

    Add files via upload

---
+++ EX2.js
@@ -0,0 +1,14 @@
+//Atividade 1
+
+let numero = 19;
+console.log("Número: "+ numero);
+if(numero % 2 == 0) console.log("Número Par");
+else console.log("Número ímpar");
+if(numero == 0) console.log("Número nulo");
+else if(numero < 0) console.log("Número negativo");
+else console.log("Número positivo");
+
+//let num = 19;
+//let mensagem = "Número " + num + " é " + ((num % 2 ==0)? "par" : "impar") + " e " + ((num < 0));
+
+//console.log(mensagem);b
